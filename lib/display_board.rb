4cb78a07a0b6d8cfdaa9a750b0f4446b90298fe2
Tien Nguyen <pri859@sudomail.com>
2018-05-25 18:51:56 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,3 +11,12 @@ end
 
 it 'prints a blank board when the board array is empty' do
       board = [" "," "," "," "," "," "," "," "," "]
+      output = capture_puts{ display_board(board) }
+            rows = output.split("\n")
+
+            expect(rows[0]).to eq("   |   |   ")
+            expect(rows[1]).to eq("-----------")
+            expect(rows[2]).to eq("   |   |   ")
+            expect(rows[3]).to eq("-----------")
+            expect(rows[4]).to eq("   |   |   ")
+          end
