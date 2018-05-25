acd468732898a5ee8371198ba52f53ad7277e58a
Tien Nguyen <pri859@sudomail.com>
2018-05-25 19:02:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 def display_board(board)
   board = [" "," "," "," "," "," "," "," "," "]
   board = ["X","X","X","X","X","X","X","X","X"]
-  board = ["0","X","X","X","X","X","X","X","X"]
+  board = ["O","X","X","X","X","X","X","X","X"]
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
